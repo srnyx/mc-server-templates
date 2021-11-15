af0944c49c02ecba7b37b07ac8e36a0efe124c4d
srnyx <roweblox@gmail.com>
2021-11-14 23:23:50 -0500

    Made server.jar for Fabric legal

--- Fabric 1.16.5/start.bat
+++ Fabric 1.16.5/start.bat
@@ -1,2 +1,5 @@
+IF NOT EXIST "server.jar" powershell -Command "(New-Object Net.WebClient).DownloadFile('https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar', 'server.jar')"
+IF NOT EXIST "server.jar" powershell -Command "Invoke-WebRequest https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar -OutFile server.jar"
+
 java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server-launch.jar nogui
 pause
