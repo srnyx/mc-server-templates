edd2a36f1a5da7c868fc0d483f7ac6e8843265ca
srnyx <roweblox@gmail.com>
2021-12-11 11:32:23 -0500

    Improved Fabric start scripts
    Shell start script now downloads the server.jar for Fabric if it doesn't exist. Code for batch script has also been greatly improved. Running either script will now download the server.jar and then start the server once it's down, not requiring the user to rerun the script.
    This may lead to much more advanced start scripts that will also download all of the plugins required.

--- Fabric 1.17.1/start.bat
+++ Fabric 1.17.1/start.bat
@@ -1,5 +1,9 @@
-IF NOT EXIST "server.jar" powershell -Command "(New-Object Net.WebClient).DownloadFile('https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar', 'server.jar')"
-IF NOT EXIST "server.jar" powershell -Command "Invoke-WebRequest https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -OutFile server.jar"
-
-IF EXIST "server.jar" java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server-launch.jar nogui
-pause
+:start
+if exist server.jar (
+    java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j2.formatMsgNoLookups=true -jar server-launch.jar nogui
+	pause
+) else (
+    powershell -Command "Invoke-WebRequest https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -OutFile server.jar"
+	echo server.jar installed! Starting server...
+	goto start
+)
