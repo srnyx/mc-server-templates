dfa37a61f38afa9526e3d8a7253e7f293615dc92
srnyx <roweblox@gmail.com>
2022-01-07 15:27:02 -0500

    [FABRIC 1.17.1] Lots of changes, read description
    - Improved .gitignore
    - Replaced Blame with Cyanide
    - Replaced World Specific View Distance with Dynamic View
    - Updated Fabric Discord Link
    - Remove FAWE Bukkit (was missplaced)
    - Added Gunpowder Utilities
    - Added back JLine for Dedicated Server
    - Updated WorldEdit
    - Removed server-launch.jar and server.jar (replaced by start.bat)

--- Fabric 1.17.1/start.bat
+++ Fabric 1.17.1/start.bat
@@ -11,7 +11,7 @@ if exist server-launch_%version%_%loader%_%installer%.jar (
     echo.
 	echo [92mStarting the server...[0m
 	echo.
-    java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j2.formatMsgNoLookups=true -jar server-launch_%version%_%loader%_%installer%.jar nogui
+    java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j.skipJansi=false -Dlog4j2.formatMsgNoLookups=true -jar server-launch_%version%_%loader%_%installer%.jar nogui
     pause
 ) else (
     if exist server-launch*.jar (
