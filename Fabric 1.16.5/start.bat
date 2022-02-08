fcb58b65fee6dfb7ca2e82c4a2d00335342c45bd
srnyx <roweblox@gmail.com>
2022-02-07 22:38:17 -0500

    [ALL] Update start files
    Now even more modernized and easier to manipulate.
    I hope that soon the start files will become so advanced it will be the only thing you need to download!

--- Fabric 1.16.5/start.bat
+++ Fabric 1.16.5/start.bat
@@ -2,34 +2,45 @@
 :: DO NOT CHANGE ANYTHING IN THIS FILE UNLESS YOU KNOW WHAT YOU'RE DOING ::
 :: DO NOT CHANGE ANYTHING IN THIS FILE UNLESS YOU KNOW WHAT YOU'RE DOING ::
 
+:: VARIABLES ::
+set project=fabric
 set version=1.16.5
-set loader=0.12.12
+set loader=0.13.1
 set installer=0.10.2
+set file=server-launch_%project%_%version%_%loader%_%installer%.jar
+set link=https://meta.fabricmc.net/v2/versions/loader/%version%/%loader%/%installer%/server/jar
 
+:: SCRIPT ::
 :start
-if exist server-launch_%version%_%loader%_%installer%.jar (
+:: Check if the latest version of %project% exists
+if exist %file% (
+    :: If it does, start the server
     echo.
 	echo [92mStarting the server...[0m
 	echo.
-    java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j2.formatMsgNoLookups=true -jar server-launch_%version%_%loader%_%installer%.jar nogui
+    java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j.skipJansi=false -Dlog4j2.formatMsgNoLookups=true -jar %file% nogui
     pause
 ) else (
+    :: If it doesn't, check if an outdated version of %project% exists
     if exist server-launch*.jar (
+	    :: If it does, delete it and go back to the start
 	    echo.
-		echo [92mDeleting outdated Fabric[0m
+		echo [92mDeleting outdated %project%[0m
 	    echo.
 		del server-launch*.jar
 	    echo.
-		echo [92mOutdated Fabric deleted[0m
+		echo [92mOutdated %project% deleted[0m
 	    echo.
 		goto start
 	) else (
+        :: If it doesn't, download the latest version of %project% and go back to the start
 	    echo.
-        echo [92mStarting download of server-launch_%version%_%loader%_%installer%.jar[0m
+        echo [92mStarting download of %file%[0m
+		echo [91mDO NOT CLOSE THIS WINDOW! If you do, delete %file% and rerun start.bat[0m
 	    echo.
-        powershell -Command "Invoke-WebRequest https://meta.fabricmc.net/v2/versions/loader/%version%/%loader%/%installer%/server/jar -OutFile server-launch_%version%_%loader%_%installer%.jar"
+        powershell -Command "Invoke-WebRequest %link% -OutFile %file%"
 	    echo.
-        echo [92mserver-launch_%version%_%loader%_%installer%.jar downloaded, continuing...[0m
+        echo [92m%file% downloaded, continuing...[0m
 	    echo.
         goto start
     )
