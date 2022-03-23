b909409286c14c708c378a05a4a40ee394d12eaf
srnyx <25808801+srnyx@users.noreply.github.com>
2022-03-22 20:30:12 -0400

    Updated Purpur 1.18.1 to 1.18.2

--- Purpur 1.18.2/start.bat
+++ Purpur 1.18.2/start.bat
@@ -4,7 +4,7 @@
 
 :: VARIABLES ::
 set project=purpur
-set version=1.18.1
+set version=1.18.2
 set build=latest
 set file=server-launch_%project%_%version%_%build%.jar
 set link=https://api.purpurmc.org/v2/%project%/%version%/%build%/download
@@ -17,7 +17,7 @@ if exist %file% (
     echo.
 	echo [92mStarting the server...[0m
 	echo.
-    java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j2.formatMsgNoLookups=true -jar %file% nogui
+    java -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j2.formatMsgNoLookups=true --add-modules=jdk.incubator.vector -jar %file% nogui
     pause
 ) else (
     :: If it doesn't, check if an outdated version of %project% exists
